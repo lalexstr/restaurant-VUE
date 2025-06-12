982468d273423ac56acc4fce4b8987bcd9c2b407
lalexstr <alexstroilov2007@icloud.com>
2025-06-13 03:15:39 +1000

    Update

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/restaurant-VUE/',
+  base: './',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+}) 
